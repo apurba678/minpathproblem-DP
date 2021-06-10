eb8f28baeaf0efc9cc81cc4ee5ac6176f677713f
apurba678 <85129407+apurba678@users.noreply.github.com>
2021-06-10 19:28:11 +0530

    Update minpath.java

--- minpath.java
+++ minpath.java
@@ -1,3 +1,13 @@
+//-----------------------------PROBLEM STATEMENT--------------------------------------
+/*Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.*/
+
+//-----------------------------PROBLEM LINK-------------------------------------------
+https://leetcode.com/problems/minimum-path-sum/
+
+
+
 public class minpath {
 
         public static void print(int[][] dp, int n) {
